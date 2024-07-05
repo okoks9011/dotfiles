cda9e2545b13bc8d90b50f2b9c941afeb57bdda7
eazy <okoks9011@gmail.com>
2024-07-05 19:26:29 +0900

    Apply upstream

---
+++ emacs/.emacs.d/early-init.el
@@ -0,0 +1,2 @@
+;; To prevent package.el loading packages prior to their init-file loading.
+(setq package-enable-at-startup nil)
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,19 +1,26 @@
 ;;
 ;; init.el start
 ;; --------------------------------------------------------------------------
-(require 'package)
+;;
+;; Initialize straight.el
+;; --------------------------------------------------------------------------
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
-
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-(eval-when-compile
-  (require 'use-package))
+(straight-use-package 'use-package)
 
 ;;
 ;; Auxiliary functions
@@ -69,27 +76,31 @@
                            ".cc")))
             (rename-file old-name new-name)))))))
 
+
 ;;
 ;; Packages
 ;; --------------------------------------------------------------------------
 (when (eq system-type 'darwin)
   (use-package exec-path-from-shell
-    :ensure t
+    :straight t
+    :init
+    (setq shell-file-name "/bin/zsh")
     :config
     (exec-path-from-shell-initialize)))
 
 (use-package color-theme-sanityinc-tomorrow
-  :ensure t
+  :straight t
   :config
   (load-theme 'sanityinc-tomorrow-bright t))
 
 (use-package org
-  :ensure t
+  :straight t
   :after ox-gfm
   :defer 7
   :bind
   (("C-c l" . org-store-link)
-   ("C-c a" . org-agenda))
+   ("C-c a" . org-agenda)
+   ("C-c b" . org-switchb))
   :init
   (setq org-hide-leading-stars t)
   (setq org-log-done t)
@@ -102,27 +113,27 @@
   (add-to-list 'org-file-apps '(directory . emacs)))
 
 (use-package org-bullets
-  :ensure t
+  :straight t
   :init
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
 (use-package magit
-  :ensure t
+  :straight t
   :bind
   (("C-x g" . magit-status)))
 
 (use-package eshell-git-prompt
-  :ensure t
+  :straight t
   :config
   (eshell-git-prompt-use-theme 'powerline))
 
 (use-package expand-region
-  :ensure t
+  :straight t
   :bind
   (("C-=" . 'er/expand-region)))
 
 (use-package web-mode
-  :ensure t
+  :straight t
   :init
   (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
   (add-hook 'web-mode-hook 'electric-pair-mode)
@@ -132,7 +143,7 @@
   (setq web-mode-code-indent-offset 2))
 
 (use-package flycheck
-  :ensure t
+  :straight t
   :init
   (add-hook 'after-init-hook 'global-flycheck-mode)
   :config
@@ -143,35 +154,33 @@
   (setq-default flycheck-disabled-checkers '(javascript-jshint)))
 
 (use-package elpy
-  :ensure t
+  :straight t
   :init
   (elpy-enable)
   :config
-  (setq elpy-rpc-python-command "python3")
   (setq elpy-modules
-        (remove 'elpy-module-flymake elpy-modules)))
+        (remove 'elpy-module-flymake elpy-modules))
+  (setq elpy-rpc-python-command "/usr/bin/python3"))
 
 (use-package f
-  :ensure t)
+  :straight t)
 
 (use-package peep-dired
-  :ensure t
+  :straight t
   :after (dired)
   :bind
   (:map dired-mode-map
         ("P" . peep-dired)))
 
 (use-package yasnippet
-  :ensure t
+  :straight t
   :init
   (add-hook 'org-mode-hook 'yas-minor-mode)
-  (add-hook 'haskell-mode-hook 'yas-minor-mode)
-  (add-hook 'go-mode-hook 'yas-minor-mode)
   :config
   (yas-reload-all))
 
 (use-package ggtags
-  :ensure t
+  :straight t
   :init
   (add-hook 'c-mode-common-hook
             (lambda ()
@@ -179,156 +188,264 @@
                 (ggtags-mode 1)))))
 
 (use-package google-c-style
-  :ensure t
+  :straight t
   :init
   (add-hook 'c-mode-common-hook (lambda () (setq c-basic-offset 4)))
   (add-hook 'c-mode-common-hook 'google-set-c-style)
   (add-hook 'c-mode-common-hook 'google-make-newline-indent))
 
 (use-package rainbow-delimiters
-  :ensure t
+  :straight t
   :init
   (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
   (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode))
 
 (use-package paredit
-  :ensure t
+  :straight t
   :init
   (add-hook 'clojure-mode-hook 'paredit-mode)
   (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
-  (add-hook 'ielm-mode-hook 'paredit-mode))
+  (add-hook 'ielm-mode-hook 'paredit-mode)
+  :bind
+  (:map paredit-mode-map
+        ("RET" . nil)
+        ("C-j" . paredit-new-line)))
 
 (use-package markdown-mode
-  :ensure t
+  :straight t
   :init
   (add-to-list 'auto-mode-alist '("\\.md$" . gfm-mode)))
 
 (use-package ivy
-  :ensure t
+  :straight t
   :config
   (setq ivy-use-virtual-buffers t)
   (setq ivy-count-format "%d/%d ")
   (ivy-mode 1))
 
 (use-package swiper
-  :ensure t
+  :straight t
   :bind
   (("C-s" . swiper)))
 
 (use-package counsel
-  :ensure t
+  :straight t
   :config
-  (counsel-mode 1))
+  (counsel-mode 1)
+  ;; Using :bind breaks counsel key map.
+  (bind-key "C-c g" 'counsel-git)
+  (bind-key "C-c j" 'counsel-git-grep))
 
 (use-package smex
-  :ensure t
+  :straight t
   :config
   (smex-initialize))
 
 (use-package avy
-  :ensure t
+  :straight t
   :bind
-  (("C-:" . avy-goto-char)))
+  (("C-:" . avy-goto-char)
+   ("C-'" . avy-goto-char-2)
+   ("M-g f" . avy-goto-line)))
 
 (use-package go-mode
-  :ensure t
+  :straight t
+  :bind
+  (("C-c m" . go-test-current-file)
+   ("C-c ." . go-test-current-test))
+  (:map copilot-completion-map
+        ("<tab>" . copilot-accept-completion)
+        ("TAB" . copilot-accept-completion)
+        ("C-<tab>" . copilot-accept-completion-by-word)
+        ("C-TAB" . copilot-accept-completion-by-word))
   :init
   (add-hook 'go-mode-hook (lambda () (setq tab-width 4)))
+  (add-hook 'go-mode-hook (lambda () (setq lsp-go-env '((GOFLAGS . "-tags=integration")))))
   (add-hook 'go-mode-hook (lambda ()
                             (setq-local whitespace-style
                                         (remove 'tabs whitespace-style))))
+  (add-hook 'go-mode-hook 'copilot-mode)
   (setq gofmt-command "goimports")
   (add-hook 'before-save-hook 'gofmt-before-save))
 
 (use-package dash-at-point
-  :ensure t
+  :straight t
   :bind
   (("C-c d" . dash-at-point))
   :init
   (add-hook 'go-mode-hook (lambda () (setq dash-at-point-docset "go"))))
 
 (use-package lsp-mode
-  :ensure t
+  :straight t
   :init
   (setq lsp-keymap-prefix "C-c p")
-  (setq lsp-file-watch-threshold 4000)
+  (setq lsp-file-watch-threshold 9000)
+  ;; (setq lsp-eldoc-enable-hover nil)
   :hook
   ((go-mode . lsp)
    (haskell-mode . lsp)
-   (fsharp-mode . lsp))
+   (fsharp-mode . lsp)
+   (lsp-mode . yas-minor-mode))
   :commands
   lsp)
 
 (use-package lsp-ui
-  :ensure t
+  :straight t
   :commands lsp-ui-mode)
 
 (use-package  lsp-ivy
-  :ensure t
+  :straight t
   :commands lsp-ivy-workspace-symbol)
 
 (use-package lsp-treemacs
-  :ensure t
+  :straight t
   :commands lsp-treemacs-erros-list)
 
 (use-package dumb-jump
-  :ensure t
+  :straight t
   :init
   (add-hook 'xref-backend-functions #'dumb-jump-xref-activate))
 
-(use-package rust-mode
-  :ensure t
-  :config
-  (setq rust-format-on-save t))
+(use-package flycheck-golangci-lint
+  :straight t
+  :hook
+  (go-mode . flycheck-golangci-lint-setup))
 
-(use-package lsp-haskell
-  :ensure t
+(use-package proof-general
+  :straight t
+  :init
+  (setq proof-splash-enable nil)
+  (setq proof-electric-terminator-enable t))
+
+(use-package which-key
+  :straight t
   :config
-  (custom-set-variables
-   '(lsp-haskell-formatting-provider "stylish-haskell"))
-  )
+  (which-key-mode +1))
+
+;; (use-package ace-window
+;;   :straight t
+;;   :bind
+;;   ([remap other-window] . ace-window))
+
+(use-package dap-mode
+  :straight t
+  :init
+  (add-hook 'dap-stopped-hook
+            (lambda (arg) (call-interactively #'dap-hydra)))
+  :config
+  (require 'dap-dlv-go)
+  (dap-register-debug-template
+   "Go Dlv Test Current Function Configuration With Integration Tag"
+   (list :type "go"
+         :request "launch"
+         :name "Test function with integration tag"
+         :mode "test"
+         :program nil
+         :args nil
+         :env '((GOFLAGS . "-tags=integration"))))
+  (dap-register-debug-template
+  "Launch managefnt with test env"
+  (list :type "go"
+        :request "launch"
+        :name "Launch Executable"
+        :mode "exec"
+        :program "../../bin/managefnt"
+        :args "-config=config/config.test.us"
+        :env '(("GOENV" . "test")))))
+
+(use-package copilot
+  :straight (:host github :repo "copilot-emacs/copilot.el" :files ("dist" "*.el")))
+
+(use-package org-roam
+  :straight t
+  :custom
+  (org-roam-directory (concat my/workspace "/wiki_logs/roam/"))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol))
+
+;; (use-package org-excalidraw
+;;   :straight (:type git :host github :repo "wdavew/org-excalidraw")
+;;   :config
+;;   (org-excalidraw-directory (concat my/workspace "/wiki_logs/excalidraw")))
 
 (use-package fsharp-mode
   :defer t
-  :ensure t)
+  :straight t)
 
-(use-package haskell-mode
-  :ensure t)
+(use-package ellama
+  :straight t
+  :init
+  (setopt ellama-keymap-prefix "C-c e")
+  (setopt ellama-lanuage "Korean")
+  (require 'llm-ollama)
+  (setopt ellama-provider
+          (make-llm-ollama
+           ;; this model should be pulled to use it
+           ;; value should be the same as you print in terminal during pull
+           :chat-model "llama3-instruct-8b:latest"
+           :embedding-model "nomic-embed-text"
+           ;; :default-chat-non-standard-params '(("num_ctx" . 8192))
+)))
+
+(use-package yafolding
+  :straight t
+  :bind (("C-c <C-return>" . yafolding-toggle-element)))
+
+(use-package pdf-tools
+  :straight t)
+
+(use-package lsp-haskell
+  :straight t)
 
 (use-package ox-gfm
-  :ensure t)
+  :straight t)
 
 (use-package sudo-edit
-  :ensure t)
+  :straight t)
 
 (use-package js2-mode
-  :ensure t)
+  :straight t)
 
 (use-package json-mode
-  :ensure t)
+  :straight t)
 
 (use-package yaml-mode
-  :ensure t)
+  :straight t)
 
 (use-package dockerfile-mode
-  :ensure t)
+  :straight t)
 
 (use-package protobuf-mode
-  :ensure t)
+  :straight t)
 
 (use-package gotest
-  :ensure t)
+  :straight t)
 
 (use-package restclient
-  :ensure t)
+  :straight t)
 
-(use-package lsp-dart
-  :ensure t)
+(use-package terraform-mode
+  :straight t)
 
 
 ;;
 ;; Custom Settings
 ;; --------------------------------------------------------------------------
+
+;; `image-type: Invalid image type ‘svg’` hotfix
+;; -----------------------------
+(add-to-list 'image-types 'svg)
 
 ;; UI Settings
 ;; ------------------------------
@@ -361,7 +478,7 @@
 (prefer-coding-system 'utf-8)
 (setq system-time-locale "C")
 
-(global-linum-mode t)
+(global-display-line-numbers-mode t)
 (unless (display-graphic-p)
   (setq linum-format "%d "))
 (add-hook 'eshell-mode-hook
@@ -378,6 +495,8 @@
 (global-whitespace-mode 1)
 
 (setq inhibit-startup-message t)
+(setq split-height-threshold 120)
+(setq split-width-threshold 240)
 
 
 ;; Language specific Settings
@@ -403,6 +522,17 @@
 (with-eval-after-load 'python
   (define-key python-mode-map (kbd "C-c C-e") 'python-pipenv-interpreter-toggle))
 
+;; LSP linter chaining
+;; ------------------------------
+(defvar-local my/flycheck-local-cache nil)
+(defun my/flycheck-checker-get (fn checker property)
+  (or (alist-get property (alist-get checker my/flycheck-local-cache))
+      (funcall fn checker property)))
+(advice-add 'flycheck-checker-get :around 'my/flycheck-checker-get)
+(add-hook 'lsp-managed-mode-hook
+          (lambda ()
+            (when (derived-mode-p 'go-mode)
+              (setq my/flycheck-local-cache '((lsp . ((next-checkers . (golangci-lint)))))))))
 
 ;; Functional Settings
 ;; ------------------------------
@@ -412,12 +542,14 @@
 
 (setq explicit-shell-file-name "/bin/bash")
 
-;; (setenv "LANG" "ko_KR.UTF-8")
+(setenv "LANG" "en_US.UTF-8")
 
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 
 (global-set-key [remap dabbrev-expand] 'hippie-expand)
+(global-set-key (kbd "C-x m") 'eshell)
+(global-set-key (kbd "C-x M-m") 'shell)
 
 
 ;; Specific
@@ -440,8 +572,6 @@
 (show-paren-mode 1)
 
 (global-set-key (kbd "C-x C-b") 'ibuffer)
-(global-set-key (kbd "C-x m") 'eshell)
-(global-set-key (kbd "C-x M-m") 'shell)
 
 (autoload 'zap-up-to-char "misc"
   "Kill up to, but not including ARGth occurrence of CHAR." t)
